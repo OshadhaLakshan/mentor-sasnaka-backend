c723c13b0b208d9617dbe1977ab8b791f221720f
oshadhalakshan <oshadhaen@gmail.com>
2025-01-28 18:08:25 +0530

    port 5000 -> 443

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 443;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
